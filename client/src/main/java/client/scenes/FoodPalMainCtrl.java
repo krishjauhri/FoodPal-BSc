e42e3ade4a7455929cc9a79bc17429baa593d586
Krish Jauhri <K.Jauhri@student.tudelft.nl>
2025-11-28 06:26:46 +0100

    Main Screen controller added

--- client/src/main/java/client/scenes/FoodPalMainCtrl.java
+++ client/src/main/java/client/scenes/FoodPalMainCtrl.java
@@ -1,4 +1,12 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class FoodPalMainCtrl {
+
+    @FXML
+    private ListView<?> colRecipeList ;
+
+    public void initialize() {}
 }
