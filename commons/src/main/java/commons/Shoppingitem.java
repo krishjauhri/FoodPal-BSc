414cfbf51db70adb041f79cd01f20b949589d3c8
Musa Mohamoud <m.s.mohamoud@student.tudelft.nl>
2025-11-28 13:46:00 +0100

    shopping

--- commons/src/main/java/commons/Shoppingitem.java
+++ commons/src/main/java/commons/Shoppingitem.java
@@ -1,4 +1,69 @@
 package commons;
 
+import java.util.Objects;
+
 public class Shoppingitem {
+    private String ingredientName;
+    private double amount;
+    private String unit;
+
+    //The name of the recipe where the item came from
+    private String sourceRecipe;
+
+    public Shoppingitem(String ingredientName, double amount, String unit, String sourceRecipe) {
+        this.ingredientName = ingredientName;
+        this.amount = amount;
+        this.unit = unit;
+        this.sourceRecipe = sourceRecipe;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public String getSourceRecipe() {
+        return sourceRecipe;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setSourceRecipe(String sourceRecipe) {
+        this.sourceRecipe = sourceRecipe;
+    }
+    @Override
+    public String toString() {
+        return getIngredientName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Shoppingitem that = (Shoppingitem) o;
+        return Double.compare(amount, that.amount) == 0 &&
+                Objects.equals(ingredientName, that.ingredientName) && Objects.equals(unit, that.unit) &&
+                Objects.equals(sourceRecipe, that.sourceRecipe);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredientName, amount, unit, sourceRecipe);
+    }
 }
--- commons/src/test/java/commons/ShoppingItemTest.java
+++ commons/src/test/java/commons/ShoppingItemTest.java
@@ -1,4 +1,58 @@
 package commons;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class ShoppingItemTest {
+    @Test
+    public void testConstructor() {
+        Shoppingitem s = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        assertEquals("Salt", s.getIngredientName());
+        assertEquals(5, s.getAmount());
+        assertEquals("g", s.getUnit());
+        assertEquals("Pizza Dough", s.getSourceRecipe());
+    }
+    @Test
+    public void testGettersAndSetters() {
+        Shoppingitem m = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        m.setIngredientName("Sugar");
+        m.setAmount(10);
+        m.setUnit("h");
+        m.setSourceRecipe("Cake");
+        assertEquals("Sugar", m.getIngredientName());
+        assertEquals(10, m.getAmount());
+        assertEquals("h", m.getUnit());
+        assertEquals("Cake", m.getSourceRecipe());
+    }
+    @Test
+    public void testEquals() {
+        Shoppingitem t = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        Shoppingitem r = new Shoppingitem("Pepper", 7, "ml", "Pasta");
+        Shoppingitem s = new Shoppingitem("Pepper", 7, "ml", "Pasta");
+        assertEquals(t, t);
+        assertEquals(t, r);
+        assertEquals(t, s);
+        assertNotEquals(r, s);
+        assertNotEquals(r, t);
+    }
+    @Test
+    public void testToString() {
+        var t = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        var real = t.toString();
+        assertEquals("Salt", real);
+        assertTrue(real.contains("Salt"));
+    }
+
+    @Test
+    public void testHashCode() {
+        Shoppingitem a = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        Shoppingitem b = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+    @Test
+    public void testNotHashCode() {
+        Shoppingitem a = new Shoppingitem("Salt", 5, "g", "Pizza Dough");
+        Shoppingitem r = new Shoppingitem("Pepper", 7, "ml", "Pasta");
+        assertNotEquals(a.hashCode(), r.hashCode());
+    }
 }
