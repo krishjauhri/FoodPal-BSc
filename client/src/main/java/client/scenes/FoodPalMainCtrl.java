41dba1a5b6282a9d86ef91cc0de61391a65f3bdf
Krish Jauhri <K.Jauhri@student.tudelft.nl>
2025-11-28 18:13:42 +0100

    adding button for addrecipe

--- client/src/main/java/client/scenes/FoodPalMainCtrl.java
+++ client/src/main/java/client/scenes/FoodPalMainCtrl.java
@@ -2,11 +2,26 @@ package client.scenes;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
 
 public class FoodPalMainCtrl {
 
     @FXML
-    private ListView<?> colRecipeList ;
+    private ListView<String> colRecipeList;
+
+    @FXML
+    private void addRecipe() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Add recipe");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Recipe name:");
+
+        dialog.showAndWait().ifPresent(name -> {
+            if (!name.isBlank()) {
+                colRecipeList.getItems().add(name);
+            }
+        });
+    }
 
     public void initialize() {}
 }
